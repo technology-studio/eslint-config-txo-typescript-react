28636b314da609e7e97ef043a3cfbcc1d5367583
Erik Slovak <erik.slovak@technologystudio.sk>
2022-09-28 15:02:28 +0200

    chore: add initial functionality

---
+++ src/configs/redux-saga.js
@@ -0,0 +1,12 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-08-10T14:08:79+02:00
+ * @Copyright: Technology Studio
+**/
+
+module.exports = {
+  rules: {
+    'redux-saga/no-yield-in-race': 'error',
+    'redux-saga/yield-effects': 'error',
+  },
+}
---
+++ test/React.ts
@@ -0,0 +1,52 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2022-08-11T14:08:90+02:00
+ * @Copyright: Technology Studio
+**/
+
+import { useEffect } from 'react'
+
+// react-hooks/rules-of-hooks
+type RulesOfHooksProps = { someProp?: string }
+export const RulesOfHooksOk = (props: RulesOfHooksProps): null => {
+  useEffect(() => {
+    if (props.someProp) {
+      // localStorage.setItem('formData', name);
+    }
+  })
+  return null
+}
+
+// export const RulesOfHooksError = (props: RulesOfHooksProps): null => {
+//   if (props.someProp) {
+//     useEffect(function persistForm () {
+//       // localStorage.setItem('formData', name);
+//     })
+//   }
+//   return null
+// }
+
+// react-hooks/exhaustive-deps
+type ExhaustiveDepsProps = { someProp?: string }
+export const ExhaustiveDepsOk = (props: ExhaustiveDepsProps): null => {
+  useEffect(() => {
+    if (props.someProp) {
+      // localStorage.setItem('formData', name);
+    }
+  }, [props.someProp])
+  return null
+}
+
+// export const ExhaustiveDepsError = (props: ExhaustiveDepsProps): null => {
+//   useEffect(() => {
+//     if (props.someProp) {
+//       // localStorage.setItem('formData', name);
+//     }
+//   }, [])
+//   return null
+// }
+
+// TODO: add tests for:
+// react/jsx-first-prop-new-line
+// react/jsx-uses-vars
+// react/display-name
