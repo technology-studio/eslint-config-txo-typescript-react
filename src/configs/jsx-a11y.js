3a2b576aacb8dbd5045fc26ef2a20c1d137bf06a
Erik Slovak <erik.slovak@technologystudio.sk>
2023-03-02 11:03:52 +0100

    chore: fix eslint issues

--- src/configs/jsx-a11y.js
+++ src/configs/jsx-a11y.js
@@ -13,11 +13,11 @@ module.exports = {
         // NOTE: If this error triggers, either disable it or add
         // your custom components, labels and attributes via these options
         // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
-        controlComponents: ['Input'],
-      },
+        controlComponents: ['Input']
+      }
     ],
     'jsx-a11y/mouse-events-have-key-events': 'error',
     'jsx-a11y/role-has-required-aria-props': 'error',
-    'jsx-a11y/role-supports-aria-props': 'error',
-  },
+    'jsx-a11y/role-supports-aria-props': 'error'
+  }
 }
