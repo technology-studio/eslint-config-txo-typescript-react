3d9ba53255f59ef89b2c77d2b8521d7d71e3f738
Erik Slovak <erik.slovak@technologystudio.sk>
2023-10-13 11:54:14 +0200

    fix: remove redux-saga plugin and config

--- test/ReduxSaga.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
- * @Date: 2022-08-11T16:08:59+02:00
- * @Copyright: Technology Studio
-**/
-
-// TODO: add tests for:
-// redux-saga/no-yield-in-race
-// redux-saga/yield-effects
