d64404c5e806ec96f612ef6eb3510eaceefa296b
Erik Slovak <erik.slovak@technologystudio.sk>
2024-11-18 22:42:22 +0100

    chore: remove changes from different branch

--- src/types/eslint-plugin-react-hooks/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'eslint-plugin-react-hooks' {
-  import type {
-    ESLint, Linter,
-  } from 'eslint'
-  export const configs: {
-    recommended: Linter.Config
-  }
-  export const rules: NonNullable<ESLint.Plugin['rules']>
-}
